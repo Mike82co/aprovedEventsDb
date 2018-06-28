6fea76720f93ccf6734c8f51c3075ab84bdd81e0
Mike Lavin <mikelavin@MacBook-Pro-5.local>
2018-06-28 09:23:45 -0600

    Added header access controll to app.js

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json());
 
 app.use("/events", event);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     const err = new Error("Not Found");
