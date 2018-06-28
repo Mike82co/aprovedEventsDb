e60ab2bc7b6026bfb581c4e23b441147169dcf2b
Mike Lavin <mikelavin@MacBook-Pro-5.local>
2018-06-28 14:40:16 -0600

    Added more feilds in the db and seed files

--- migrations/20180627111358_approved-events.js
+++ migrations/20180627111358_approved-events.js
@@ -2,10 +2,14 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('event', table => {
         table.increments();
         table.text('date');
-        table.text('venue');
         table.text('artist');
+        table.text('venue');
+        table.text('contactName')
+        table.text('phone');
+        table.text('email');
+        table.text('website');
+        table.text('musicSample');
         table.text('image');
-        table.text('musicSample')
         table.text('description');
     })
 };
