4e672ae5b512e9bb20ef3b6d0bff6c7c5d795b38
Mike Lavin <mikelavin@MacBook-Pro-5.local>
2018-06-29 14:27:47 -0600

    updated the seed infor with new artists

--- events.js
+++ events.js
@@ -7,38 +7,33 @@ module.exports = [{
     phone:"303-333-3333",
     email:"rumtum@aol.com",
     website:"http://www.wikipedia.com",
-    musicSample: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/105119837&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    musicSample: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/487462143&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
     image: "https://images1.westword.com/imager/moving-to-denver-has-been-highly-fortuitou/u/745xauto/5122142/9195074.0.jpg",
     description: "Denver's headiest beatmaker blurs the lines between dance, organic world music, and old-fashioned dope beats."
 },
 {
     id: 2,
     date: "06-28-2018",
-    artist:"Slipknot",
+    artist:"Pueblo Escobar",
     venue: "Ogden",
     contactName: "Peter Chun",
     phone:"555-555-5555",
     email:"no@no.com",
-    website:"http://www.yahoo.com",
-    musicSample:"https://soundcloud.com/nycpride/episode-56-dj-alex-acosta",
-    image: "https://images-na.ssl-images-amazon.com/images/I/81lrF8AR%2BtL._SX522_.jpg",
-    description: "Murder Inc.",
+    website:"https://www.facebook.com/puebloescobar303/",
+    musicSample:"https://www.youtube.com/embed/hZKi_Q8dbkY?ecver=2",
+    image: "https://f4.bcbits.com/img/a0635741903_16.jpg",
+    description: "No shocker that Luke Fairchild, Joe Ramirez and Arj Narayan would start a heavy noise-rock band. And no surprise that the music pretty much lives up to a name that suggests crushing experiences in grimy locales. And if that doesn't sound appealing for some reason, Pueblo Escobar is also a great, high-energy rock-and-roll band.",
 },
 {
     id: 3,
     date: "06-15-2018",
-    artist: "Lady Gaga",
+    artist: "Chuck Roy",
     venue: "Filmore",
-    contactName:"Jack Black",
+    contactName:"Chuck Roy",
     phone:"404-909-8877",
     email:"yes@yes.com",
-    website:"http://www.aol.com",
-    musicSample:"https://www.youtube.com/embed/bESGLojNYSo?ecver=2",
-    image: "https://eonlinethumbs-a.akamaihd.net/images/647/471/glam_2018grammys_ladygaga_268370_560x315_1148015171950.jpg",
-
+    website:"https://chuckroy.com/",
+    musicSample:"https://www.youtube.com/embed/y62AzEECTtk?ecver=2",
+    image: "https://chuckroy.com/wp-content/uploads/2016/10/ChuckRoy-com-MainPage.jpg",
     description: "Pop star",
 }]
-
-
-
-
